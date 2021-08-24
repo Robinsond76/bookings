f948193244dbf2253b92a1675c49631618f17b57
Robinson Souza <Robinsond76@gmail.com>
2021-08-24 19:13:04 -0400

    Working on server side validation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/robinsond76/bookings/internal/config"
+	"github.com/robinsond76/bookings/internal/forms"
 	"github.com/robinsond76/bookings/internal/models"
 	"github.com/robinsond76/bookings/internal/render"
 )
@@ -53,7 +54,13 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{})
+	render.RenderTemplate(w, r, "make-reservation.page.tmpl", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
+
 }
 
 func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
--- internal/models/templatedata.go
+++ internal/models/templatedata.go
@@ -1,5 +1,7 @@
 package models
 
+import "github.com/robinsond76/bookings/internal/forms"
+
 //Template data holds data sent from handler to templates
 type TemplateData struct {
 	StringMap map[string]string
@@ -10,4 +12,5 @@ type TemplateData struct {
 	Flash     string
 	Warning   string
 	Error     string
+	Form      *forms.Form
 }
